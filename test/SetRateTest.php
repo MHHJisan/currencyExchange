5288c69e535625f36163d7a4e1cc6efd7b8dfba4
Mhh Haque <mhhaque4@gmail.com>
2024-02-12 17:02:36 +0600

    Alhamdulillah

--- test/SetRateTest.php
+++ test/SetRateTest.php
@@ -22,7 +22,37 @@ class SetRateTest extends TestCase{
         $this->assertEquals(109650, $test_get_Dest_Amount[1]);
     }
 
-    
+
+    public function testsetRate() {
+
+        $dbuser = 'postgres';
+        $dbpass = 'jisan';
+        $dbhost = 'localhost';
+        $dbname = 'exchangeRate';
+
+        $connect = new PDO("pgsql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+        $test_query = "INSERT INTO exchange_rates (source_currency, destination_currency, rate)
+                 VALUES ('ABC', 'XYZ', 12345.456)";
+        $test_sql = $connect->query($test_query);
+
+        $query = "SELECT * FROM exchange_rates 
+                 WHERE source_currency = 'ABC' 
+                 AND destination_currency = 'XYZ' AND rate = 12345.456";
+        
+        $sql = $connect->query($query);
+
+        $data = $sql->fetch(PDO::FETCH_ASSOC);
+
+        $this->assertEquals('ABC', $data['source_currency'], "Incorrect source currency");
+        $this->assertEquals('XYZ', $data['destination_currency'], "Incorrect destination currency");
+        $this->assertEquals(12345.456, $data['rate'], "Incorrect rate");
+
+        // Clean up: Delete test data from the database
+        $delete_query = "DELETE FROM exchange_rates WHERE source_currency = 'ABC' 
+                          AND destination_currency = 'XYZ' AND rate = 12345.456";
+        $delete_sql = $connect->query($delete_query);
+
+    }    
 }
 
 
