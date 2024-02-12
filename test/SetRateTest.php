50d4941fbc805f11452e37c455289e2c970db9f9
Mhh Haque <mhhaque4@gmail.com>
2024-02-12 07:32:39 +0600

    correcting SetRateTest.php

--- test/SetRateTest.php
+++ test/SetRateTest.php
@@ -1,22 +1,26 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-include '../Model/getRate.php';
-include '../Model/getAmount.php';
+include '../Model/GetRate.php';
+include '../Model/GetAmount.php';
 class SetRateTest extends TestCase{
 
     function getRate() {
-        $test_get_Rate = getRate('USD', 'BDT');
+
+        $get_rate = new GetRate();
+        $test_get_Rate = $get_rate->getRate('USD', 'BDT');
         $this->assertEquals(109.65, $test_get_Rate);
     }
 
     function getDestAmount() {
-        $test_get_Dest_Amount = getDestAmount('USD', 'BDT', 1000);
-        $this->assertEquals(109650, $test_get_Dest_Amount[0]);
-        $this->assertEquals(109.65, $test_get_Dest_Amount[1]);
+
+        $get_Dst_Amount = new GetAmount();
+        $test_get_Dest_Amount = $get_Dst_Amount->getDestAmount('USD', 'BDT', 1000);
+        $this->assertEquals(109650, $test_get_Dest_Amount);
+        // $this->assertEquals(109.65, $test_get_Dest_Amount[1]);
     }
 
-    function setRate();
+    // function setRate();
 }
 
 
