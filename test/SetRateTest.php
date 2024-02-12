d1d926104c03f78cde7dc2d55cd6c4fc93519a94
Mhh Haque <mhhaque4@gmail.com>
2024-02-12 15:55:06 +0600

    hopefully everything is fine, Alhamdulillah

--- test/SetRateTest.php
+++ test/SetRateTest.php
@@ -5,22 +5,24 @@ include_once './Model/GetAmount.php';
 use PHPUnit\Framework\TestCase;
 class SetRateTest extends TestCase{
 
-    function getRate() {
+    
+    public function testgetRate() {
 
         $get_rate = new GetRate();
         $test_get_Rate = $get_rate->getRate('USD', 'BDT');
         $this->assertEquals(109.65, $test_get_Rate);
+        
     }
 
-    function getDestAmount() {
+    function testgetDestAmount() {
 
         $get_Dst_Amount = new GetAmount();
         $test_get_Dest_Amount = $get_Dst_Amount->getDestAmount('USD', 'BDT', 1000);
-        $this->assertEquals(109650, $test_get_Dest_Amount);
-        // $this->assertEquals(109.65, $test_get_Dest_Amount[1]);
+        $this->assertEquals(109.65, $test_get_Dest_Amount[0]);
+        $this->assertEquals(109650, $test_get_Dest_Amount[1]);
     }
 
-    // function setRate();
+    
 }
 
 
